1a2895989cb687f7860104738df81e6f3895c950
samuelselasi <samelsekasi@gmail.com>
2023-03-19 14:42:53 -0700

    Update lists.h with prototype of question 2

--- c/lists.h
+++ c/lists.h
@@ -21,6 +21,8 @@ size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
+int check_cycle(listint_t *list);
 listint_t *insert_node(listint_t **head, int number);
+int is_palindrome(listint_t **head);
 
 #endif /* LISTS_H */
